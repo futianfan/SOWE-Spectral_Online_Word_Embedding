8be0a86c97427e75f89f681f5084569a18ca1046
futianfan <futianfan@gmail.com>
2018-08-20 00:50:29 -0400

    comments

--- estimate_weight_prob.m
+++ estimate_weight_prob.m
@@ -3,9 +3,9 @@ len = length(c_marginal);  % c_marginal(i) is count for i-th word.
 weight_prob = zeros(len,1);
 weight_prob(len) = 10;
 for i = 2:len
-    j = len - i + 1;  ## from bottom to top
+    j = len - i + 1;  %% from bottom to top
     weight_prob(j) = max(weight_prob(j+1),c_marginal(j)) ;
-### make it at least >= its right neibours
+%% make it at least >= its right neibours
 end
 weight_prob = weight_prob / sum(weight_prob);  %% sum to 1
 
